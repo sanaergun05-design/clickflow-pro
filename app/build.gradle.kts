428a317ba45f64c473286eacfc277f7dcec537a5
sanaergun05-design <sanaergun05@gmail.com>
2026-08-23 20:23:47 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.clickflowpro.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.clickflowpro.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "1.0"
     }
