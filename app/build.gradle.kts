54d46fc752b44be48b4e47dea700749c886ad097
sanaergun05-design <sanaergun05@gmail.com>
2026-09-12 13:21:09 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.clickflowpro.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     signingConfigs {
